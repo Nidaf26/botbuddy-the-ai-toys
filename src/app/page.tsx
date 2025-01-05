b6b57192bec6c93d0f76ef243329af0308001b1a
Nidaf26 <nidafatima2602@gmail.com>
2025-01-05 18:39:11 +0500

    debugged errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Header  from "@/components/Header";
 import Heroparallax from "@/components/here-parallax";
-import { div } from "framer-motion/client";
 
 export default function Home() {
   return (
