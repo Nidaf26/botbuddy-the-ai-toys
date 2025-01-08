6cfa64728a69f1812d84701f3ee68d9f2584b647
Nidaf26 <nidafatima2602@gmail.com>
2025-01-09 00:47:54 +0500

    updated names of website

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link';
 function Categories() {
   return (
     <nav >
-     <span className='bg-slate-50 h-[50px] flex justify-around items-center font-bold'>
-        <Link href={""}>Little Explorers (3 to 8) </Link>
-        <Link href={"/youngInnovaters"}>Young Innovators (9 to 14)</Link>
-        <Link href={"/"}>Tech Trialblazzer(15 to 18)</Link>
+     <span className='bg-slate-50 h-[50px] flex justify-around items-center font-bold msm:text-sm text-center ssm:text-sm xsm:text-sm'>
+        <Link href={""}>Little Explorers <br /> (3 to 8) </Link>
+        <Link href={"/youngInnovaters"}>Young Innovators <br /> (9 to 14)</Link>
+        <Link href={"/"}>Tech Trialblazzer <br /> (15 to 18)</Link>
      </span> 
     </nav>
   )
